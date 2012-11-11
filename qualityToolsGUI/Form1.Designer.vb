0bfea41d693803a521fe138437ea4f0fba52ab68
jatorres <angelitorresn@hotmail.com>
2012-11-11 16:06:04 -0500

    Cambio de Lenguaje aplicación de C# a VB
    Se debe eliminar los archivos dentro del directorio "qualityToolsGUI"
    excepto el "README.md"

---
+++ qualityToolsGUI/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.groupBox1 = New System.Windows.Forms.GroupBox()
+        Me.linkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.textBox2 = New System.Windows.Forms.TextBox()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.groupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'groupBox1
+        '
+        Me.groupBox1.Controls.Add(Me.linkLabel1)
+        Me.groupBox1.Controls.Add(Me.button1)
+        Me.groupBox1.Controls.Add(Me.textBox2)
+        Me.groupBox1.Controls.Add(Me.textBox1)
+        Me.groupBox1.Controls.Add(Me.label3)
+        Me.groupBox1.Controls.Add(Me.label2)
+        Me.groupBox1.Location = New System.Drawing.Point(13, 45)
+        Me.groupBox1.Name = "groupBox1"
+        Me.groupBox1.Size = New System.Drawing.Size(303, 134)
+        Me.groupBox1.TabIndex = 3
+        Me.groupBox1.TabStop = False
+        Me.groupBox1.Text = "Datos"
+        '
+        'linkLabel1
+        '
+        Me.linkLabel1.AutoSize = True
+        Me.linkLabel1.Location = New System.Drawing.Point(50, 92)
+        Me.linkLabel1.Name = "linkLabel1"
+        Me.linkLabel1.Size = New System.Drawing.Size(153, 26)
+        Me.linkLabel1.TabIndex = 5
+        Me.linkLabel1.TabStop = True
+        Me.linkLabel1.Text = "Olvido su usuario o contraseña" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Clic para recordar"
+        Me.linkLabel1.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(209, 86)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(73, 25)
+        Me.button1.TabIndex = 4
+        Me.button1.Text = "Ingresar"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'textBox2
+        '
+        Me.textBox2.Location = New System.Drawing.Point(100, 49)
+        Me.textBox2.Name = "textBox2"
+        Me.textBox2.Size = New System.Drawing.Size(182, 20)
+        Me.textBox2.TabIndex = 3
+        Me.textBox2.UseSystemPasswordChar = True
+        '
+        'textBox1
+        '
+        Me.textBox1.Location = New System.Drawing.Point(100, 23)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(182, 20)
+        Me.textBox1.TabIndex = 2
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label3.Location = New System.Drawing.Point(17, 50)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(77, 16)
+        Me.label3.TabIndex = 1
+        Me.label3.Text = "Contraseña"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.Location = New System.Drawing.Point(39, 24)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(55, 16)
+        Me.label2.TabIndex = 0
+        Me.label2.Text = "Usuario"
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(109, 9)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(125, 24)
+        Me.label1.TabIndex = 2
+        Me.label1.Text = "Bienvenidos"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(327, 190)
+        Me.Controls.Add(Me.groupBox1)
+        Me.Controls.Add(Me.label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.groupBox1.ResumeLayout(False)
+        Me.groupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents groupBox1 As System.Windows.Forms.GroupBox
+    Private WithEvents linkLabel1 As System.Windows.Forms.LinkLabel
+    Private WithEvents button1 As System.Windows.Forms.Button
+    Private WithEvents textBox2 As System.Windows.Forms.TextBox
+    Private WithEvents textBox1 As System.Windows.Forms.TextBox
+    Private WithEvents label3 As System.Windows.Forms.Label
+    Private WithEvents label2 As System.Windows.Forms.Label
+    Private WithEvents label1 As System.Windows.Forms.Label
+
+End Class
