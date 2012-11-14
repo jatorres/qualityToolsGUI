32ebc48a6adcd4eeda296d2acaef2ee3375a273a
rodolfo moncada duran <rodartmon@hotmail.com>
2012-11-13 19:18:14 -0500

    pp

--- qualityToolsGUI/mapaprocesos.vb
+++ qualityToolsGUI/mapaprocesos.vb
@@ -15,4 +15,8 @@
         direccioncomercial.Show()
         Me.Hide()
     End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+
+    End Sub
 End Class
